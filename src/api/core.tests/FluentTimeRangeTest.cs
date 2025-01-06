f134ee88a370bcb55649735575d5df7e853af485
mrstefangrimm <motionssystems@grimm.com>
2025-01-06 21:41:28 +0100

    More unit tests.

--- src/api/core.tests/FluentTimeRangeTest.cs
+++ src/api/core.tests/FluentTimeRangeTest.cs
@@ -21,4 +21,14 @@ public class FluentTimeRangeTest : FluentTestBase
     .Build().Value
     .Should().Contain(item => item.Commit == "abc" || item.Commit == "bcd")
     .And.NotContain(item => item.Commit == "cde");
+
+  [Fact]
+  public void TimeRange_FirstAndThirdCommit_ReturnsExpectedElements()
+    => _testRecords
+    .TimeRange()
+    .In(DateTime.MinValue, DateTime.Parse("2024-12-20T19:35:00Z", _fmt))
+    .And(DateTime.Parse("2024-12-22T18:00:00Z", _fmt), DateTime.MaxValue)
+    .Build().Value
+    .Should().Contain(item => item.Commit == "abc" || item.Commit == "cde")
+    .And.NotContain(item => item.Commit == "bcd");
 }
