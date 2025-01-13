7d9972b3e9d712df58e08c1bde6688d84ba57a39
mrstefangrimm <motionssystems@grimm.com>
2025-01-13 21:49:38 +0100

    Add examples.

---
+++ examples/Giana.API.Fluent/Program.cs
@@ -0,0 +1,19 @@
+﻿using Giana.Api.Analysis.Ranking;
+using Giana.Api.Core.Fluent;
+using Giana.Api.Load;
+using System;
+
+const string gitExePath = @"C:\Program Files\Git\bin\git.exe";
+const string gitRepository = "https://github.com/dotnet/wpf.git";
+
+var repo = await GitRepository.CreateAsync(gitRepository, gitExePath);
+
+var records = await repo.LogAsync();
+
+records = records
+  .TimeRange().In(DateTime.Now.AddMonths(-6), DateTime.Now)
+  .Rename().Author("Thomas Goulet", "ThomasGoulet73")
+  .Build();
+
+var authorRanking = AuthorRankingCalculations.CreateAuthorRankingSorted(records);
+AuthorRankingActions.WriteAsCsv(authorRanking, Console.Out);
---
+++ examples/Giana.Api.Functional/Program.cs
@@ -0,0 +1,18 @@
+﻿using Giana.Api.Analysis.Ranking;
+using Giana.Api.Core;
+using Giana.Api.Load;
+using System;
+
+const string gitExePath = @"C:\Program Files\Git\bin\git.exe";
+const string gitRepository = "https://github.com/dotnet/wpf.git";
+
+string tempClone = Actions.CreateCloneFromUri(gitRepository, gitExePath);
+string repoName = Actions.RequestRepositoryName(tempClone, gitExePath);
+
+var records = Actions.RequestGitLog(tempClone, repoName, gitExePath);
+
+records = records.WithTimeRange(DateTime.Now.AddMonths(-6), DateTime.Now);
+records = records.RenameAuthor("Thomas Goulet", "ThomasGoulet73");
+
+var authorRanking = AuthorRankingCalculations.CreateAuthorRankingSorted(records);
+AuthorRankingActions.WriteAsCsv(authorRanking, Console.Out);
