9cfbcb3d54509c25ba48d2868295d7c6a82ffb50
mrstefangrimm <motionssystems@grimm.com>
2025-01-26 17:07:46 +0100

    Update examples.

--- examples/Giana.App.Query/Program.cs
+++ examples/Giana.App.Query/Program.cs
@@ -1,18 +1,18 @@
 ﻿using Giana.App.Shared;
 using System;
 
-const string gitExePath = @"C:\Git2\bin\git.exe";
-const string gitRepository = "https://github.com/mrstefangrimm/giana.git";
+const string gitExePath = @"C:\Program Files\Git\bin\git.exe";
+const string gitRepository = "https://github.com/dotnet/wpf.git";
 
 var query = new Query
 {
   Sources = [gitRepository],
   Analyzer = "author-ranking",
   OutputFormat = "csv",  
-  Renames = [new Author() { To = "Stefan Grimm", From = "mrstefangrimm" }]
+  Renames = [new Author() { To = "Thomas Goulet", From = "ThomasGoulet73" }]
 };
 
 var routine = query.CreateRoutine();
 routine.OutputWriter = Console.Out;
 
-await Giana.App.Shared.Actions.ExecuteAsync(routine, () => gitExePath, 10000);
+await Actions.ExecuteAsync(routine, () => gitExePath, 60000);
