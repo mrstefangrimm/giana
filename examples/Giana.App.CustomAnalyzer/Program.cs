8c9ffc96845cd1994d2dd0dddc6709e6ed8a535b
Stefan Grimm <Stefan.Grimm@varian.com>
2025-01-30 15:35:13 +0100

    Use Giana.Cmd 0.0.2, Giana.Api 0.0.7.

--- examples/Giana.App.CustomAnalyzer/Program.cs
+++ examples/Giana.App.CustomAnalyzer/Program.cs
@@ -1,18 +1,21 @@
 ﻿using Giana.App.Shared;
 using System;
 
-const string gitExePath = @"C:\Program Files\Git\bin\git.exe";
-const string gitRepository = "https://github.com/dotnet/wpf.git";
+const string gitExePath = @"C:\Git2\bin\git.exe";
+const string gitRepository = "https://github.com/mrstefangrimm/giana.git";
 
 var query = new Query
 {
   Sources = [gitRepository],
-  Analyzer = "author-ranking",
+  Analyzer = "custom-analysis",
   OutputFormat = "csv",
   Renames = [new Author() { To = "Thomas Goulet", From = "ThomasGoulet73" }],
   TimeRanges = [new TimePeriod() { Begin = DateTime.Now.AddMonths(-6), End = DateTime.Now }]
 };
 
-var routine = query.CreateRoutine();
+var analyzers = Calculations.GetDefaultAnalyzers();
+analyzers = analyzers.WithCustomAnalyzers("Giana.App.CustomAnalyzer");
+
+var routine = query.CreateRoutine(analyzers);
 
 await routine.ExecuteAsync(gitExePath, Console.Out, TimeSpan.FromSeconds(60));
