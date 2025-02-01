0575e48249d5d3b2783d10ad13453e31f7c9093f
mrstefangrimm <motionssystems@grimm.com>
2025-02-01 09:21:53 +0100

    Update CustomAnalyzer example.

--- examples/Giana.App.CustomAnalyzer/Program.cs
+++ examples/Giana.App.CustomAnalyzer/Program.cs
@@ -1,14 +1,14 @@
 ﻿using Giana.App.Shared;
 using System;
 
-const string gitExePath = @"C:\Git2\bin\git.exe";
-const string gitRepository = "https://github.com/mrstefangrimm/giana.git";
+const string gitExePath = @"C:\Program Files\Git\bin\git.exe";
+const string gitRepository = "https://github.com/dotnet/wpf.git";
 
 var query = new Query
 {
   Sources = [gitRepository],
   Analyzer = "custom-analysis",
-  OutputFormat = "csv",
+  OutputFormat = "html",
   Renames = [new Author() { To = "Thomas Goulet", From = "ThomasGoulet73" }],
   TimeRanges = [new TimePeriod() { Begin = DateTime.Now.AddMonths(-6), End = DateTime.Now }]
 };
